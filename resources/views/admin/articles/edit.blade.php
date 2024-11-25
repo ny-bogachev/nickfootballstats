07bec44cde0de895a0a5d079f2f3da2e9125feef
Nikita Bogachev <nikitabogachev12@gmail.com>
2024-11-25 16:29:17 +0100

    create and edit design improvements

--- resources/views/admin/articles/edit.blade.php
+++ resources/views/admin/articles/edit.blade.php
@@ -1,31 +1,30 @@
-<x-site-layout>
+<x-site-layout title="Edit match article">
 
-    <form action="{{route('admin.articles.update', $article)}}" method="post">
+    <form action="{{route('admin.articles.update', $article)}}" method="post" class="w-2/3 border border-gray-300 p-4">
         @method('PUT')
         @csrf
 
-        <label for="title">Competitors:</label><br/>
-        <input name="title" value="{{old('title', $article->title)}}">
-        @error('title')<div class="text-red-500">{{$message}}</div> @enderror
+        <label for="title" class="block text-xs font-semibold uppercase">Competitors:</label><br/>
+        <input name="title" value="{{old('title', $article->title)}}" class="w-2/3 p-1 rounded-lg border border-gray-200 @error('title') border-red-500 @enderror">
+        @error('title')<div class="text-red-500 text-xs">{{$message}}</div> @enderror
 
         <br/><br/>
 
-        <label for="score">Score:</label><br/>
-        <input name="score" value="{{old('score', $article->score)}}">
-        @error('score')<div class="text-red-500">{{$message}}</div> @enderror
+        <label for="score" class="block text-xs font-semibold uppercase">Score:</label><br/>
+        <input name="score" value="{{old('score', $article->score)}}" class="w-2/3 p-1 rounded-lg border border-gray-200 @error('score') border-red-500 @enderror">
+        @error('score')<div class="text-red-500 text-sx">{{$message}}</div> @enderror
 
         <br/><br/>
 
-        <label for="stadia_id">Stadium ID:</label><br/>
-        <input name="stadia_id" value="{{old('stadia_id', $article->stadia_id)}}">
-        @error('stadia_id')<div class="text-red-500">{{$message}}</div> @enderror
+        <label for="stadia_id" class="block text-xs font-semibold uppercase">Stadium ID:</label><br/>
+        <input name="stadia_id" value="{{old('stadia_id', $article->stadia_id)}}" class="w-2/3 p-1 rounded-lg border border-gray-200 @error('stadia_id') border-red-500 @enderror">
+        @error('stadia_id')<div class="text-red-500 text-xs">{{$message}}</div> @enderror
 
         <br/><br/>
 
-        <div>
-            <a href="{{route('admin.articles.index')}}">Undo</a>
-            <br/>
-            <button type="submit">Submit</button>
+        <div class="w-full flex justify-end gap-x-8">
+            <a href="{{route('admin.articles.index')}}" class="text-xs text-gray-700 bg-gray-300 hover:bg-gray-200 px-4 py-2 rounded uppercase">Undo</a>
+            <button type="submit" class="text-xs text-green-700 bg-green-300 hover:bg-green-200 px-4 py-2 rounded uppercase">Submit match article</button>
         </div>
     </form>
 
--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -1,6 +1,7 @@
-<x-site-layout>
-
-    <a href="{{route('admin.articles.create')}}">Create new match article</a>
+<x-site-layout title="All Match Articles">
+    <div class="flex justify-center mb-4">
+        <a class="text-m text-green-700 bg-green-300 px-1 py-.5 rounded uppercase" href="{{route('admin.articles.create')}}">Create match article</a>
+    </div>
 
     @if(session()->has('success'))
         <div class="bg-green-100 text-green-500 p-2">
@@ -8,14 +9,28 @@
         </div>
     @endif
 
-    <ul class="list-disc pl-4">
+    <table class="table-auto w-full border border-gray-300">
+        <thead>
+        <tr class="bg-gray-300">
+            <th>Title</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
         @foreach($articles as $article)
-            <li>
-                {{$article->title}}
-                <a href="{{route('admin.articles.edit', $article)}}">edit</a>
-                <form action="{{route('admin.articles.destroy', $article)}}" method="post">@method('delete')@csrf<button type="submit">delete</button></form>
-            </li>
+            <tr class="hover:bg-gray-200 border-b border-gray-200">
+                <td><a href="{{route('admin.articles.show', $article)}}">{{$article->title}}</a></td>
+                <td class="flex gap-x-4 justify-center items-center">
+                    <a href="{{route('admin.articles.edit', $article)}}" class="text-xs text-blue-700 bg-blue-300 px-1 py-.5 rounded uppercase">Edit</a>
+                    <form action="{{route('admin.articles.destroy', $article)}}" method="post">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="text-xs text-red-700 bg-red-300 px-1 py-.5 rounded uppercase">Delete</button>
+                    </form>
+                </td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
 
 </x-site-layout>
